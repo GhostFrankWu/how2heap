36456d93bfcd3a4ebba98347463ae2c8a00060c0
Frank Wu <56958034+GhostFrankWu@users.noreply.github.com>
2021-11-09 22:51:30 +0800

    学习第二个文件

--- calc_tcache_idx.c
+++ calc_tcache_idx.c
@@ -3,21 +3,27 @@
 #include <string.h>
 #include <malloc.h>
 
-
+/*这是Ptmalloc2中chunk的基本结构*/
 struct malloc_chunk {
 
   size_t      mchunk_prev_size;  /* Size of previous chunk (if free).  */
-  size_t      mchunk_size;       /* Size in bytes, including overhead. */
+  size_t      mchunk_size;       /* 当前chunk的大小 */
+  /* 因为chunk一定是8字节对齐，所以mchunk_size的低3位为0，它们作为flag位分别表示：
+   * PREV_INUSE：前一个chunk是否被分配，1表示已经分配，0表示被释放
+   * IS_MMAPPED：当前chunk是否是mmap分配的，1表示是mmap分配的，0表示是malloc分配的
+   * NON_MAIN_ARENA：当前chunk是否是main_arena（主线程）的，1表示属于main_arena
+   */
 
+  /* 在small bin和large bin中使用。  */
   struct malloc_chunk* fd;         /* double links -- used only if free. */
   struct malloc_chunk* bk;
 
-  /* Only used for large blocks: pointer to next larger size.  */
+  /* 只在large bin中使用，形成排序链表。  */
   struct malloc_chunk* fd_nextsize; /* double links -- used only if free. */
   struct malloc_chunk* bk_nextsize;
 };
 
-/* The corresponding word size.  */
+/* 当前系统中一个 word 的大小  */
 #define SIZE_SZ (sizeof (size_t))
 
 #define MALLOC_ALIGNMENT (2 * SIZE_SZ < __alignof__ (long double) \
@@ -48,25 +54,25 @@ int main()
 {
     unsigned long long req;
     unsigned long long tidx;
-	fprintf(stderr, "This file doesn't demonstrate an attack, but calculates the tcache idx for a given chunk size.\n");
-	fprintf(stderr, "The basic formula is as follows:\n");
+	fprintf(stderr, "这段代码在给定chunk size时计算了tcache idx，不演示攻击。\n");
+	fprintf(stderr, "计算公式如下:\n");
     fprintf(stderr, "\tIDX = (CHUNKSIZE - MINSIZE + MALLOC_ALIGNMENT - 1) / MALLOC_ALIGNMENT\n");
-    fprintf(stderr, "\tOn a 64 bit system the current values are:\n");
+    fprintf(stderr, "\t在当前的系统（x64）下，这些值是:\n");
     fprintf(stderr, "\t\tMINSIZE: 0x%lx\n", MINSIZE);
     fprintf(stderr, "\t\tMALLOC_ALIGNMENT: 0x%lx\n", MALLOC_ALIGNMENT);
-    fprintf(stderr, "\tSo we get the following equation:\n");
+    fprintf(stderr, "\t所以我们得出如下公式:\n");
     fprintf(stderr, "\tIDX = (CHUNKSIZE - 0x%lx) / 0x%lx\n\n", MINSIZE-MALLOC_ALIGNMENT+1, MALLOC_ALIGNMENT);
-    fprintf(stderr, "BUT be AWARE that CHUNKSIZE is not the x in malloc(x)\n");
-    fprintf(stderr, "It is calculated as follows:\n");
+    fprintf(stderr, "但请注意 CHUNKSIZE 并不是 malloc(x)中的x\n");
+    fprintf(stderr, "x的计算方式如下:\n");
     fprintf(stderr, "\tIF x + SIZE_SZ + MALLOC_ALIGN_MASK < MINSIZE(0x%lx) CHUNKSIZE = MINSIZE (0x%lx)\n", MINSIZE, MINSIZE);
     fprintf(stderr, "\tELSE: CHUNKSIZE = (x + SIZE_SZ + MALLOC_ALIGN_MASK) & ~MALLOC_ALIGN_MASK) \n");
     fprintf(stderr, "\t=> CHUNKSIZE = (x + 0x%lx + 0x%lx) & ~0x%lx\n\n\n", SIZE_SZ, MALLOC_ALIGN_MASK, MALLOC_ALIGN_MASK);
     while(1) {
-        fprintf(stderr, "[CTRL-C to exit] Please enter a size x (malloc(x)) in hex (e.g. 0x10): ");
+        fprintf(stderr, "[按下CTRL-C退出] 请输入16进制大小x用于malloc(x)，比如0x10: ");
         scanf("%llx", &req);
         tidx = usize2tidx(req);
         if (tidx > 63) {
-            fprintf(stderr, "\nWARNING: NOT IN TCACHE RANGE!\n");
+            fprintf(stderr, "\nWARNING: 超出TCACHE范围!\n");
         }
         fprintf(stderr, "\nTCache Idx: %llu\n", tidx);
     }
