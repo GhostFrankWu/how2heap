fe8035741e9ca280bc33505d96317e3fc444fb7f
Frank Wu <56958034+GhostFrankWu@users.noreply.github.com>
2021-11-18 23:29:04 +0800

    演示最基础的double-free

--- glibc_2.31/fastbin_dup.c
+++ glibc_2.31/fastbin_dup.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+/*
+ * Ptmalloc2会验证当前释放的chunk是否和上次释放的chunk是同一个，如果是，则说明发生了double free，
+ * 这个逻辑很直接，也很容易绕过，演示如下：
+ */
 
 int main()
 {
 	setbuf(stdout, NULL);
 
-	printf("This file demonstrates a simple double-free attack with fastbins.\n");
+	printf("本文件使用fastbins演示了一个简单的double-free工具.\n");
 
-	printf("Fill up tcache first.\n");
+	printf("首先填满8个tcache\n");
 	void *ptrs[8];
 	for (int i=0; i<8; i++) {
 		ptrs[i] = malloc(8);
@@ -17,34 +21,34 @@ int main()
 		free(ptrs[i]);
 	}
 
-	printf("Allocating 3 buffers.\n");
+	printf("分配三个内存.\n");
 	int *a = calloc(1, 8);
 	int *b = calloc(1, 8);
 	int *c = calloc(1, 8);
 
-	printf("1st calloc(1, 8): %p\n", a);
-	printf("2nd calloc(1, 8): %p\n", b);
-	printf("3rd calloc(1, 8): %p\n", c);
+	printf("第一次1st分配calloc(1, 8)到: %p\n", a);
+	printf("第二次2nd分配calloc(1, 8)到: %p\n", b);
+	printf("第三次3rd分配calloc(1, 8)到: %p\n", c);
 
-	printf("Freeing the first one...\n");
+	printf("Free第一个内存...\n");
 	free(a);
 
-	printf("If we free %p again, things will crash because %p is at the top of the free list.\n", a, a);
+	printf("如果我们再次free %p, 程序会崩溃，因为 %p 在freelist的第一个.\n", a, a);
 	// free(a);
 
-	printf("So, instead, we'll free %p.\n", b);
+	printf("所以我们来free第二块内存 %p.\n", b);
 	free(b);
 
-	printf("Now, we can free %p again, since it's not the head of the free list.\n", a);
+	printf("现在我们就可以再次free %p 了，因为他不是“上次”释放的内存了.\n", a);
 	free(a);
 
-	printf("Now the free list has [ %p, %p, %p ]. If we malloc 3 times, we'll get %p twice!\n", a, b, a, a);
+	printf("现在free list为 [ %p, %p, %p ]. 如果我们malloc三次，我们会得到两次 %p !\n", a, b, a, a);
 	a = calloc(1, 8);
 	b = calloc(1, 8);
 	c = calloc(1, 8);
-	printf("1st calloc(1, 8): %p\n", a);
-	printf("2nd calloc(1, 8): %p\n", b);
-	printf("3rd calloc(1, 8): %p\n", c);
+	printf("第一次1st分配calloc(1, 8)到: %p\n", a);
+	printf("第二次2nd分配calloc(1, 8)到: %p\n", b);
+	printf("第三次3rd分配calloc(1, 8)到: %p\n", c);
 
 	assert(a == c);
 }
